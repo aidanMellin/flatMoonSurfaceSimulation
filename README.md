10da98a46ef5fcb574ed5bf6a70d83f72457da20
Aidan <aidan.mellin@gmail.com>
2023-11-30 11:45:09 -0500

    3d perspective and basic camera controls

--- README.md
+++ README.md
@@ -67,7 +67,10 @@ Add Controls for Viewing the Terrain
 Implement basic controls to navigate around the terrain. This could be as simple as rotating the scene or moving a camera.
 
 #TODO
-- [ ] Perspective for Plane
+- [x] Perspective for Plane
+    - [ ] Fix rotation to be around camera instead of around origin
+    - [ ] Perspective traveling for W/S doesn't regenerate perspective to maintain illusion
+    - [ ] Bound it so that the points stay within the screen if possible.
 - [ ] 2D Texture Mapping
 - [ ] 2D to 3D Textures
 - [ ] Lighting
--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+// Global variables
+let gl;
+let shaderProgram;
+let positionBuffer;
+let projectionMatrix;
+let viewMatrix;
+let uProjectionMatrixLocation;
+let fov = 60; // Field of view in degrees
+let cameraPosition = [0, .3, 1.6];
+let lookAtPoint = [0, 1, -1];
+let upVector = [0, 1, 0];
+
 const vertexShaderSource = `
     attribute vec4 aVertexPosition;
-    void main() {
-        gl_PointSize = 2.0; // Increase the point size for visibility
-        gl_Position = aVertexPosition;
-    }`;
+    uniform mat4 uProjectionMatrix;
 
+    void main() {
+        gl_PointSize = 2.0; // Set this to a visible size
+        gl_Position = uProjectionMatrix * aVertexPosition;
+    }`;
 
 const fragmentShaderSource = `
     void main() {
@@ -13,19 +26,17 @@ const fragmentShaderSource = `
 
 function init() {
     const canvas = document.getElementById('glCanvas');
-    const gl = canvas.getContext('webgl');
+    gl = canvas.getContext('webgl');
 
     if (!gl) {
         alert('Unable to initialize WebGL. Your browser may not support it.');
         return;
     }
 
-    // Load and compile the shaders
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-    // Create the shader program
-    const shaderProgram = gl.createProgram();
+    shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
@@ -35,14 +46,65 @@ function init() {
         return;
     }
 
-    // Create the buffer for vertex positions
-    const vertices = createGrid(100, 100); // Create a 100x100 grid
-    const positionBuffer = gl.createBuffer();
+    projectionMatrix = mat4.create();
+    viewMatrix = mat4.create();
+
+    positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    let vertices = createGrid(100, 100);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-    // Start the render loop
-    render(gl, shaderProgram, positionBuffer);
+    uProjectionMatrixLocation = gl.getUniformLocation(shaderProgram, 'uProjectionMatrix');
+
+    setupCamera();
+    setupEventListeners();
+
+    render();
+}
+
+function setupCamera() {
+    let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    mat4.perspective(projectionMatrix, (fov * Math.PI) / 180, aspect, 0.1, 100.0);
+    mat4.lookAt(viewMatrix, cameraPosition, lookAtPoint, upVector);
+    mat4.multiply(projectionMatrix, projectionMatrix, viewMatrix);
+}
+
+function setupEventListeners() {
+    window.addEventListener('keydown', function(event) {
+        switch (event.key) {
+            case 'w':
+                cameraPosition[2] -= 0.1; // Move forward
+                break;
+            case 's':
+                cameraPosition[2] += 0.1; // Move backward
+                break;
+            case 'a':
+                cameraPosition[0] -= 0.1; // Move left
+                break;
+            case 'd':
+                cameraPosition[0] += 0.1; // Move right
+                break;
+            case 'ArrowUp':
+                fov = Math.min(fov + 1, 120); // Increase FOV
+                break;
+            case 'ArrowDown':
+                fov = Math.max(fov - 1, 1); // Decrease FOV
+                break;
+            // Add more cases as needed
+        }
+        updateCamera();
+    });
+
+    window.addEventListener('resize', function() {
+        if (resizeCanvasToDisplaySize(gl.canvas)) {
+            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+            setupCamera();
+        }
+    });
+}
+
+function updateCamera() {
+    setupCamera();
 }
 
 function loadShader(gl, type, source) {
@@ -61,34 +123,49 @@ function loadShader(gl, type, source) {
 
 function createGrid(rows, columns) {
     let vertices = [];
-    for (let y = 0; y <= rows; y++) {
+    for (let z = 0; z <= rows; z++) {
         for (let x = 0; x <= columns; x++) {
-            // Normalize x, y to [-1, 1]
-            vertices.push(2 * x / columns - 1, 2 * y / rows - 1, 0);
+            // Spread vertices along X and Z, with Y as the up-axis
+            vertices.push(
+                2 * x / columns - 1, // X
+                0,                   // Y (up-axis, flat)
+                2 * z / rows - 1     // Z
+            );
         }
     }
     return vertices;
 }
 
 
-function render(gl, shaderProgram, positionBuffer) {
-    // Set clear color to black, fully opaque
+
+function render() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     gl.useProgram(shaderProgram);
-
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     const vertexPosition = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
     gl.enableVertexAttribArray(vertexPosition);
     gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
 
-    // Draw the grid (as points for now)
-    gl.drawArrays(gl.POINTS, 0, (100 + 1) * (100 + 1)); // Adjust count based on grid size
+    gl.uniformMatrix4fv(uProjectionMatrixLocation, false, projectionMatrix);
 
-    requestAnimationFrame(() => render(gl, shaderProgram, positionBuffer));
+    gl.drawArrays(gl.POINTS, 0, (100 + 1) * (100 + 1));
+
+    requestAnimationFrame(render);
+}
+
+
+function resizeCanvasToDisplaySize(canvas) {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    if (canvas.width !== width || canvas.height !== height) {
+        canvas.width = width;
+        canvas.height = height;
+        return true;
+    }
+    return false;
 }
 
 // Call the init function when the window loads
